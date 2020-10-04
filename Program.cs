e48a02ee268fe66d09f9017750956a4b3ff5c3ed
James Long <jameslong595@gmail.com>
2020-10-04 02:19:12 -0400

    solved problem 5, improved problems 3 & 4

--- Program.cs
+++ Program.cs
@@ -106,70 +106,110 @@ namespace Assignment2_Fall2020
             //} // end of split array while loop
 
 
-//            // square values of an array and return sorted squares
-//            while (true)
-//            {
-//                try
-//                {
-//                    // collect data from user, validate, and convert to an integer array
-//                    Console.WriteLine(@"
-//Enter a comma-separated array of integers sorted in non-decreasing order.
-//The minimum allowable length is 1.
-//The maximum allowable length is 10,000.
-//The minimum allowable value is -10,000.
-//The maximum allowable value is 10,000.
-//");
-//                    string input = Console.ReadLine();
-//                    string[] strArr = input.Split(",");
-//                    if (strArr.Length > 10000)
-//                    {
-//                        throw new ArgumentOutOfRangeException();
-//                    }
-//                    int[] array3 = new int[strArr.Length];
-//                    int num;
-//                    for (int i = 0; i < array3.Length; i++)
-//                    {
-//                        num = Convert.ToInt32(strArr[i]);
-//                        array3[i] = num;
-//                        if (i > 0 && array3[i] < array3[i - 1])
-//                        {
-//                            throw new ArgumentOutOfRangeException();
-//                        }
-//                        if (array3[i] < -10000 || array3[i] > 10000)
-//                        {
-//                            throw new ArgumentOutOfRangeException();
-//                        }
-//                    } // end of for loop
-//                    int[] result2 = SortedSquares(array3);
-//                    if (result2 == null)
-//                    {
-//                        break;
-//                    }
-//                    else
-//                    {
-//                        Console.WriteLine("\n" + string.Join(", ", result2));
-//                        Console.WriteLine("\nPress any key to continue ...\n");
-//                        Console.ReadKey();
-//                        break;
-//                    }
-//                } // end of try block
-//                catch
-//                {
-//                    Console.WriteLine("\nInvalid input ...\n");
-//                } // end of catch block
-//            } // end of sorted squares while loop
+            //// square values of an array and return sorted squares
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        // collect data from user, validate, and convert to an integer array
+            //        Console.WriteLine(@"
+            //Enter a comma-separated array of integers sorted in non-decreasing order.
+            //The minimum allowable length is 1.
+            //The maximum allowable length is 10,000.
+            //The minimum allowable value is -10,000.
+            //The maximum allowable value is 10,000.
+            //");
+            //        string input = Console.ReadLine();
+            //        string[] strArr = input.Split(",");
+            //        if (strArr.Length > 10000)
+            //        {
+            //            throw new ArgumentOutOfRangeException();
+            //        }
+            //        int[] array3 = new int[strArr.Length];
+            //        int num;
+            //        for (int i = 0; i < array3.Length; i++)
+            //        {
+            //            num = Convert.ToInt32(strArr[i]);
+            //            array3[i] = num;
+            //            if (i > 0 && array3[i] < array3[i - 1])
+            //            {
+            //                throw new ArgumentOutOfRangeException();
+            //            }
+            //            if (array3[i] < -10000 || array3[i] > 10000)
+            //            {
+            //                throw new ArgumentOutOfRangeException();
+            //            }
+            //        } // end of for loop
+            //        int[] result2 = SortedSquares(array3);
+            //        if (result2 == null)
+            //        {
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("\n[" + string.Join(", ", result2) + "]");
+            //            Console.WriteLine("\nPress any key to continue ...\n");
+            //            Console.ReadKey();
+            //            break;
+            //        }
+            //    } // end of try block
+            //    catch
+            //    {
+            //        Console.WriteLine("\nInvalid input ...\n");
+            //    } // end of catch block
+            //} // end of sorted squares while loop
 
 
-            Console.WriteLine("Question 5");
-            int[] nums1 = { 4, 2, 2, 4 };
-            int[] nums2 = { 2, 2 };
-            int[] intersect1 = Intersect(nums1, nums2);
-            // Write code to print the result array here
+            //// find and print intersection of two arrays
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        // collect data from user and convert to the first integer array
+            //        Console.WriteLine("Enter the first comma-separated array of integers in any order:\n");
+            //        string input1 = Console.ReadLine();
+            //        string[] strArr1 = input1.Split(",");
+            //        int[] nums1 = new int[strArr1.Length];
+            //        int num1;
+            //        for (int i = 0; i < nums1.Length; i++)
+            //        {
+            //            num1 = Convert.ToInt32(strArr1[i]);
+            //            nums1[i] = num1;
+            //        } // end of for loop
+            //        // collect data from user and convert to the second integer array
+            //        Console.WriteLine("\nEnter the second comma-separated array of integers in any order:\n");
+            //        string input2 = Console.ReadLine();
+            //        string[] strArr2 = input2.Split(",");
+            //        int[] nums2 = new int[strArr2.Length];
+            //        int num2;
+            //        for (int i = 0; i < nums2.Length; i++)
+            //        {
+            //            num2 = Convert.ToInt32(strArr2[i]);
+            //            nums2[i] = num2;
+            //        } // end of for loop
+            //        int[] intersect1 = Intersect(nums1, nums2);
+            //        if (intersect1 == null)
+            //        {
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("\n[" + string.Join(", ", intersect1) + "]");
+            //            Console.WriteLine("\nPress any key to continue ...\n");
+            //            Console.ReadKey();
+            //            break;
+            //        }
+            //    } // end of try block
+            //    catch
+            //    {
+            //        Console.WriteLine("\nInvalid input ...\n");
+            //    } // end of catch block
+            //} // end of intersetion while loop
 
 
-            //Console.WriteLine("Question 6");
-            //int[] arr = new int[] { 1, 2, 2, 1, 1, 3 };
-            //Console.WriteLine(UniqueOccurrences(arr));
+            Console.WriteLine("Question 6");
+            int[] arr = new int[] { 1, 2, 2, 1, 1, 3 };
+            Console.WriteLine(UniqueOccurrences(arr));
 
 
             //Console.WriteLine("Question 7");
@@ -216,6 +256,7 @@ namespace Assignment2_Fall2020
                 Console.ReadKey();
             } // end of catch block
         } // end of method PrintPatternAnyComplexity
+        // if n = number of lines then O(n)
 
 
         public static void PrintPatternLinearComplexity(int n)
@@ -247,6 +288,7 @@ namespace Assignment2_Fall2020
                 Console.ReadKey();
             } // end of catch block
         } // end of method PrintPatternLinearComplexity
+        // if n = number of lines then O(n)
 
 
         public static int LongestSubSeq(int[] nums)
@@ -283,21 +325,22 @@ namespace Assignment2_Fall2020
                 return -1;
             } // end of catch block
         } // end of method LongestSubSeq
+        // if n = length of input array then O(n)
 
 
         public static void PrintTwoParts(int[] array2)
         {
             // Given an array of integers greater than zero, find if it is possible to split it
             // in two subarrays (without reordering the elements), such that the sum of the two
-            // subarrays is the same. Print the two subarrays.
+            // subarrays is the same. Print the two subarrays or False.
             try
             {
                 int sum1 = 0;
-                int sum2 = 0;
+                int sum2 = array2.Sum();
                 for (int i = 0; i < array2.Length; i++)
                 {
                     sum1 += array2[i];
-                    sum2 = array2.Sum() - sum1;
+                    sum2 -= array2[i];
                     if (array2.Length == 1)
                     {
                         Console.WriteLine("\nFalse");
@@ -309,7 +352,7 @@ namespace Assignment2_Fall2020
                     {
                         int[] subArr1 = array2[0..(i + 1)];
                         int[] subArr2 = array2[(i + 1)..array2.Length];
-                        Console.WriteLine("\n" + string.Join(", ", subArr1) + "\n" + string.Join(", ", subArr2) + "\n");
+                        Console.WriteLine("\n[" + string.Join(", ", subArr1) + "]" + "\n[" + string.Join(", ", subArr2) + "]\n");
                         Console.WriteLine("\nPress any key to continue ...\n");
                         Console.ReadKey();
                         break;
@@ -330,6 +373,7 @@ namespace Assignment2_Fall2020
                 Console.ReadKey();
             } // end of catch block
         } // end of method PrintTwoParts
+        // if n = length of input array then O(n)
 
 
         public static int[] SortedSquares(int[] A)
@@ -345,7 +389,7 @@ namespace Assignment2_Fall2020
                 // find first non-negative element
                 for (int i = 0; i < A.Length; i++)
                 {
-                    if (A[i] >= 0)
+                    if (A[i] >= 0 || i == (A.Length-1))
                     {
                         r = i;
                         break;
@@ -353,7 +397,7 @@ namespace Assignment2_Fall2020
                 } // end of for loop
                 // initialize left side index tracker
                 int l = r - 1;
-                // start in middle and work outward in both directions
+                // start somewhere in the middle and work outward in both directions
                 for (int i = 0; i < B.Length; i++)
                 {
                     if (l == -1 && r < B.Length)
@@ -387,21 +431,52 @@ namespace Assignment2_Fall2020
                 return null;
             } // end of catch block
         } // end of method SortedSquares
+        // if n = length of input array then O(2n) which simplifies to O(n)
 
 
         public static int[] Intersect(int[] nums1, int[] nums2)
         {
+            // Given two arrays, write a function to compute their intersection. 
+            // Each element in the result should appear as many times as it shows in both arrays.
+            // The result can be in any order.
             try
             {
-                // Write your code here
-            }
-            catch
+                Dictionary<int, int> dict = new Dictionary<int, int>();
+                List<int> lst = new List<int>();
+                foreach (int i in nums1)
+                {
+                    if (dict.TryAdd(i, 1))
+                    { }
+                    else
+                    {
+                        dict[i]++;
+                    }
+                } // end of foreach loop
+                foreach (int i in nums2)
+                {
+                    if (dict.ContainsKey(i) && dict[i] > 0)
+                    {
+                        lst.Add(i);
+                        dict[i]--;
+                    }
+                }
+                int[] intersection = new int[lst.Count];
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    intersection[i] = lst[i];
+                }
+                return intersection;
+            } // end of try block
+            catch (Exception e)
             {
-                throw;
-            }
-
-            return new int[] { };
+                Console.WriteLine($"\nAn {e.GetType().Name} error occured while computing Intersect()");
+                Console.WriteLine("\nPress any key to continue ...\n");
+                Console.ReadKey();
+                return null;
+            } // end of catch block
         } // end of method Intersect
+        // if n = total length of both input arrays then O(n)
+        // if n = length of first array & k = length of second array then O(n+k) which simplifies to O(n)
 
 
         public static bool UniqueOccurrences(int[] arr)
